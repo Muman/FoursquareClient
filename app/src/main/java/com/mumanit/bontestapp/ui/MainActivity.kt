69e352f6d45698bbac25ca967f1de629ff02ee7c
Piotr Muciek <piotr.muciek@tvp.pl>
2019-11-07 21:47:45 +0100

    Refactor MainActivity to use ViewModel instead od MVP pattern.

--- app/src/main/java/com/mumanit/bontestapp/ui/MainActivity.kt
+++ app/src/main/java/com/mumanit/bontestapp/ui/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import butterknife.BindView
@@ -13,9 +15,9 @@ import com.google.android.gms.location.LocationRequest
 import com.mumanit.bontestapp.R
 import com.mumanit.bontestapp.app.App
 import com.mumanit.bontestapp.domain.model.VenueData
-import com.mumanit.bontestapp.ui.venues.VenuesContract
 import com.mumanit.bontestapp.ui.venues.VenuesListAdapter
-import com.mumanit.bontestapp.ui.venues.VenuesListPresenter
+import com.mumanit.bontestapp.ui.venues.VenuesViewModel
+import com.mumanit.bontestapp.ui.viewmodel.ViewModelFactory
 import permissions.dispatcher.*
 import pl.charmas.android.reactivelocation.ReactiveLocationProvider
 import rx.Subscription
@@ -23,16 +25,16 @@ import java.util.*
 import javax.inject.Inject
 
 @RuntimePermissions
-class MainActivity : AppCompatActivity(), VenuesContract.VenuesListView {
+class MainActivity : AppCompatActivity() {
 
     @BindView(R.id.rvVenuesList)
     internal lateinit var rvVenuesList: RecyclerView
 
     @Inject
-    internal lateinit var presenter: VenuesListPresenter
+    internal lateinit var locationProvider: ReactiveLocationProvider
 
     @Inject
-    internal lateinit var locationProvider: ReactiveLocationProvider
+    internal lateinit var viewModelFactory: ViewModelFactory
 
     @Inject
     internal lateinit var locationUpdateConfig: LocationRequest
@@ -40,6 +42,8 @@ class MainActivity : AppCompatActivity(), VenuesContract.VenuesListView {
     internal lateinit var venuesListAdapter: VenuesListAdapter
     internal lateinit var venuesListLayoutManager: LinearLayoutManager
     internal var locationSubscription: Subscription? = null
+
+    private lateinit var viewModel: VenuesViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,7 +53,15 @@ class MainActivity : AppCompatActivity(), VenuesContract.VenuesListView {
         injectDependencies()
         initAdapter()
         initRecycler()
-        presenter.attach(this)
+        initViewModel()
+    }
+
+    private fun initViewModel() {
+        viewModel = ViewModelProviders.of(this, viewModelFactory).get(VenuesViewModel::class.java)
+    }
+
+    private fun startObservingViewModel() {
+        viewModel.venues.observe(this, this.venuesObserver)
     }
 
     private fun initAdapter() {
@@ -82,7 +94,9 @@ class MainActivity : AppCompatActivity(), VenuesContract.VenuesListView {
     fun startReceivingLocationUpdates() {
         locationSubscription = locationProvider
                 .getUpdatedLocation(locationUpdateConfig)
-                .subscribe({ presenter.loadVenues() }, { })
+                .subscribe({
+                    viewModel.loadVenues()
+                }, { })
     }
 
     @OnShowRationale(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
@@ -104,30 +118,28 @@ class MainActivity : AppCompatActivity(), VenuesContract.VenuesListView {
         Toast.makeText(this, "never ask again", Toast.LENGTH_SHORT).show()
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        presenter.detach()
-    }
-
     private fun injectDependencies() {
-        App.getAppComponent(this).inject(this)
+        App.getAppComponent(this)?.inject(this)
     }
 
-    override fun showVenuesList(venueDataList: List<VenueData>?) {
+    private fun showVenuesList(venueDataList: List<VenueData>?) {
         venuesListAdapter.data = venueDataList
         venuesListAdapter.notifyDataSetChanged()
         Toast.makeText(this, "data loaded", Toast.LENGTH_LONG).show()
     }
 
-    override fun showLoading() {
+    private fun showLoading() {
 
     }
 
-    override fun hideLoading() {
+    private fun hideLoading() {
 
     }
 
-    override fun showError() {
+    private fun showError() {
         Toast.makeText(this, "loading error", Toast.LENGTH_LONG).show()
     }
+
+    private val venuesObserver = Observer<List<VenueData>> { t -> showVenuesList(t) }
+
 }
